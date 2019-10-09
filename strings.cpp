28ce4a21d280716bbb514d1004e7f84b44326963
xiaji <jwjwei@gmail.com>
2019-10-09 20:06:03 +0800

    switch and enum class

--- strings.cpp
+++ strings.cpp
@@ -3,8 +3,18 @@
 
 int main() {
   char english[] = "A book holds a house of gold";
-  char16_t chinese[] = u"\u4e66\u4e2d\u81ea\u6709\u9ec4\u91d1\u5c4b";
+  //char16_t chinese[] = u"\u4e66\u4e2d\u81ea\u6709\u9ec4\u91d1\u5c4b";
   printf("English : %s;\n", english);
-  printf("Chinese : %S.\n", chinese);
+  //printf("Chinese : %S.\n", chinese);
+  char alphabet[27];
+  for (int i = 0; i < 26; i++) {
+    alphabet[i] = i + 97;
+  }
+  //alphabet[26] = 0;
+  printf("%s\n", alphabet);
+  for (int i = 0; i < 26; i++) {
+    alphabet[i] = i + 65;
+  }
+  printf("%s\n", alphabet);
   return 0;
 }
