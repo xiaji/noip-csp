20084ad0b14b5d0a1edfac9d77b1aaf3f8adc3bb
xiaji <jwjwei@gmail.com>
2019-11-11 12:08:22 +0800

    kruskal and union-find finished

--- noip-2013/truck/mine.cpp
+++ noip-2013/truck/mine.cpp
@@ -25,10 +25,85 @@ struct IO {
     }
     return x * sign;
   }
+
+  inline void open() {
+    freopen("truck.in", "w", stdin);
+    freopen("truck.out", "r", stdout);
+  }
+
+  inline void close() {
+    fclose(stdin);
+    fclose(stdout);
+  }
 }io;
 
-int ancestor[MXN][31], cost[MXN][31];
 //union-find method, they need rank (whether this rank can reuse? just try)
+int fa[MXN], deep[MXN];
+struct union_find {
+  int find(int x) {
+    return fa[x] == x ? x : fa[x] = find(fa[x]);
+  }
 
-//adj vector array to store graph
-vector<pair<int, int>> adj[]
+  bool is_union(int x, int y) {
+    return find(x) == find(y);
+  }
+
+  void unite(int x, int y) {
+    int xfa = find(x);
+    int yfa = find(y);
+    if (xfa == yfa) return;
+    if (deep[xfa] < deep[yfa]) {
+      fa[xfa] = yfa;
+    } else {
+      fa[yfa] = xfa;
+      if (deep[xfa] == deep[yfa]) deep[xfa]++;
+    }
+  }
+}uf;
+
+// find max spanning tree
+struct Edge {
+  int s, d, w;
+  bool operator < (const Edge& tmp) const {
+    return w > tmp.w;
+  }
+};
+
+int n, m;
+
+void kruskal(Edge* e, vector<pair<int, int> >* adj) {
+  int cnt = 0;
+  _rep(i, 0, m) {
+    int u = e[i].s, v = e[i].d, w = e[i].w; 
+    //int sfa = uf.find(u);
+    //int dfa = uf.find(v);
+    if (uf.is_union(u, v)) continue;
+    fa[v] = u;
+    adj[v].push_back(make_pair(u, w));
+    adj[u].push_back(make_pair(v, w));
+    cnt++;
+    if (cnt >= n - 1) break;
+  }
+}
+
+// adj vector array to store graph
+// vector<pair<int, int> > adj[MXN];
+
+int main() {
+  io.open();
+  n = io.read();
+  m = io.read();
+  Edge e[m];
+  _rep(i, 0, m) {
+    e[i].s = io.read();
+    e[i].d = io.read();
+    e[i].w = io.read();
+  }
+  sort(e, e + m);
+  vector<pair<int, int> > adj[n + 1];
+  kruskal(e, adj);
+  io.close();
+  return 0;
+}
+
+
